471ec06e7a25ff683e9fbaeeb482f6486fa47f56
Jeremy Vanseader <madoxlabs@gmail.com>
2014-12-11 00:59:57 -0500

    done JS fbx binary parser

--- webGL/FBXPacker/fbxload.bin.worker.js
+++ webGL/FBXPacker/fbxload.bin.worker.js
@@ -1,8 +1,7 @@
+importScripts('pako.js');
 
 onmessage = function(oEvent)
 {
-  log("Got model data, size " + oEvent.data.length);
-  log("Starting");
   process(oEvent.data);
 }
 
@@ -18,48 +17,57 @@ function log(msg)
 {
   postMessage({ type: 2, result: indent + msg });
 }
+function debug(msg)
+{
+//  postMessage({ type: 2, result: indent + msg });
+}
 
 var indent = "";
 
+var intbuffer = new ArrayBuffer(Uint8Array.BYTES_PER_ELEMENT * 4);
+var intbytes = new Uint8Array(intbuffer);
+var intview = new Uint32Array(intbuffer);
+
 function getInteger(cursor)
 {
-  var i = 0, v;
-  i = i * 256; v = cursor.data[cursor.offset + 3]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 2]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 1]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 0]; i += v;
+  intbytes[0] = cursor.data[cursor.offset + 0];
+  intbytes[1] = cursor.data[cursor.offset + 1];
+  intbytes[2] = cursor.data[cursor.offset + 2];
+  intbytes[3] = cursor.data[cursor.offset + 3];
   cursor.offset += 4;
-  return i;
+  return intview[0];
 }
 
-var floatbuffer = new ArrayBuffer(4);
+var floatbuffer = new ArrayBuffer(Uint8Array.BYTES_PER_ELEMENT * 4);
+var floatbytes = new Uint8Array(floatbuffer);
+var floatview = new Float32Array(floatbuffer);
 
 function getFloat(cursor)
 {
-  floatbuffer[0] = cursor.data[cursor.offset + 3];
-  floatbuffer[1] = cursor.data[cursor.offset + 2];
-  floatbuffer[2] = cursor.data[cursor.offset + 1];
-  floatbuffer[3] = cursor.data[cursor.offset + 0];
-  var floatview = new Float32Array(floatbuffer);
+  floatbytes[0] = cursor.data[cursor.offset + 0];
+  floatbytes[1] = cursor.data[cursor.offset + 1];
+  floatbytes[2] = cursor.data[cursor.offset + 2];
+  floatbytes[3] = cursor.data[cursor.offset + 3];
   cursor.offset += 4;
   return floatview[0];
 }
 
-var wfloatbuffer = new ArrayBuffer(8);
+var wfloatbuffer = new ArrayBuffer(Uint8Array.BYTES_PER_ELEMENT * 8);
+var wfloatbytes = new Uint8Array(wfloatbuffer);
+var wfloatview = new Float64Array(wfloatbuffer);
 
 function getWideFloat(cursor)
 {
-  wfloatbuffer[0] = cursor.data[cursor.offset + 7];
-  wfloatbuffer[1] = cursor.data[cursor.offset + 6];
-  wfloatbuffer[2] = cursor.data[cursor.offset + 5];
-  wfloatbuffer[3] = cursor.data[cursor.offset + 4];
-  wfloatbuffer[4] = cursor.data[cursor.offset + 3];
-  wfloatbuffer[5] = cursor.data[cursor.offset + 2];
-  wfloatbuffer[6] = cursor.data[cursor.offset + 1];
-  wfloatbuffer[7] = cursor.data[cursor.offset + 0];
-  var floatview = new Float64Array(wfloatbuffer);
+  wfloatbytes[0] = cursor.data[cursor.offset + 0];
+  wfloatbytes[1] = cursor.data[cursor.offset + 1];
+  wfloatbytes[2] = cursor.data[cursor.offset + 2];
+  wfloatbytes[3] = cursor.data[cursor.offset + 3];
+  wfloatbytes[4] = cursor.data[cursor.offset + 4];
+  wfloatbytes[5] = cursor.data[cursor.offset + 5];
+  wfloatbytes[6] = cursor.data[cursor.offset + 6];
+  wfloatbytes[7] = cursor.data[cursor.offset + 7];
   cursor.offset += 8;
-  return floatview[0];
+  return wfloatview[0];
 }
 
 function getShort(cursor)
@@ -98,19 +106,22 @@ function getLongString(cursor)
   return result;
 }
 
+var wintbuffer = new ArrayBuffer(Uint8Array.BYTES_PER_ELEMENT * 4);
+var wintbytes = new Uint8Array(wintbuffer);
+var wintview = new Uint32Array(wintbuffer);
+
 function getWideInteger(cursor)
 {
-  var i = 0, v;
-  i = i * 256; v = cursor.data[cursor.offset + 7]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 6]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 5]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 4]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 3]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 2]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 1]; i += v;
-  i = i * 256; v = cursor.data[cursor.offset + 0]; i += v;
+  wintbytes[0] = cursor.data[cursor.offset + 0];
+  wintbytes[1] = cursor.data[cursor.offset + 1];
+  wintbytes[2] = cursor.data[cursor.offset + 2];
+  wintbytes[3] = cursor.data[cursor.offset + 3];
+//  wintbytes[4] = cursor.data[cursor.offset + 4];
+//  wintbytes[5] = cursor.data[cursor.offset + 5];
+//  wintbytes[6] = cursor.data[cursor.offset + 6];
+//  wintbytes[7] = cursor.data[cursor.offset + 7];
   cursor.offset += 8;
-  return i;
+  return wintview[0];
 }
 
 function getSmallInteger(cursor)
@@ -136,15 +147,71 @@ function getData(cursor)
   return " (" + len + " bytes)";
 }
 
+function getArrayFloat(cursor)
+{
+  var len = getInteger(cursor);
+  var encoding = getInteger(cursor);
+  var encodelen = getInteger(cursor);
+
+  var mycursor = cursor;
+  if (encoding) {
+    var zip = new Uint8Array(encodelen);
+    for (var i = 0; i < encodelen; ++i) zip[i] = cursor.data[cursor.offset + i];
+    var unzip = pako.inflate(zip);
+    cursor.offset += encodelen;
+    mycursor = { data: unzip, offset: 0 }
+  }
+
+  var ret = [];
+  for (var i = 0; i < len; ++i) {
+    ret.push(getFloat(mycursor));
+  }
+  return ret;
+}
+
+function getArrayWideFloat(cursor)
+{
+  var len = getInteger(cursor);
+  var encoding = getInteger(cursor);
+  var encodelen = getInteger(cursor);
+
+  var mycursor = cursor;
+  if (encoding) {
+    var zip = new Uint8Array(encodelen);
+    for (var i = 0; i < encodelen; ++i) zip[i] = cursor.data[cursor.offset + i];
+    var unzip = pako.inflate(zip);
+    cursor.offset += encodelen;
+    mycursor = { data: unzip, offset: 0 }
+  }
+
+  var ret = [];
+  for (var i = 0; i < len; ++i) {
+    ret.push(getWideFloat(mycursor));
+  }
+  return ret;
+}
+
 function getArrayInteger(cursor)
 {
   var len = getInteger(cursor);
   var encoding = getInteger(cursor);
   var encodelen = getInteger(cursor);
 
-  cursor.offset += encodelen ? encodelen : (len*4);
+  var mycursor = cursor;
+  if (encoding)
+  {
+    var zip = new Uint8Array(encodelen);
+    for (var i = 0; i < encodelen; ++i) zip[i] = cursor.data[cursor.offset + i];
+    var unzip = pako.inflate(zip);
+    cursor.offset += encodelen;
+    mycursor = { data: unzip, offset: 0 }
+  }
 
-  return " array is type: " + encoding + " with # items: " + len;
+  var ret = [];
+  for (var i = 0; i < len; ++i) {
+    ret.push(getInteger(mycursor));
+  }
+  return ret;
 }
 
 function getArrayWideInteger(cursor)
@@ -153,8 +220,20 @@ function getArrayWideInteger(cursor)
   var encoding = getInteger(cursor);
   var encodelen = getInteger(cursor);
 
-  cursor.offset += encodelen ? encodelen : (len * 8);
-  return " array is type: " + encoding + " with # items: " + len;
+  var mycursor = cursor;
+  if (encoding) {
+    var zip = new Uint8Array(encodelen);
+    for (var i = 0; i < encodelen; ++i) zip[i] = cursor.data[cursor.offset + i];
+    var unzip = pako.inflate(zip);
+    cursor.offset += encodelen;
+    mycursor = { data: unzip, offset: 0 }
+  }
+
+  var ret = [];
+  for (var i = 0; i < len; ++i) {
+    ret.push(getWideInteger(mycursor));
+  }
+  return ret;
 }
 
 function getArrayBool(cursor)
@@ -163,11 +242,23 @@ function getArrayBool(cursor)
   var encoding = getInteger(cursor);
   var encodelen = getInteger(cursor);
 
-  cursor.offset += encodelen ? encodelen : len;
-  return " array is type: " + encoding + " with # items: " + len;
+  var mycursor = cursor;
+  if (encoding) {
+    var zip = new Uint8Array(encodelen);
+    for (var i = 0; i < encodelen; ++i) zip[i] = cursor.data[cursor.offset + i];
+    var unzip = pako.inflate(zip);
+    cursor.offset += encodelen;
+    mycursor = { data: unzip, offset: 0 }
+  }
+
+  var ret = [];
+  for (var i = 0; i < len; ++i) {
+    ret.push(getBool(mycursor));
+  }
+  return ret;
 }
 
-function parseObjectRecord(cursor)
+function parseObjectRecord(obj, cursor)
 {
   var buf = "";
 
@@ -175,63 +266,94 @@ function parseObjectRecord(cursor)
   var end = getInteger(cursor);    // end offset
   var len = getInteger(cursor);    // num properties
   var bytelen = getInteger(cursor); // properties byte length
-  buf += getString(cursor);
+
+  var name = getString(cursor);
+  buf += name;
+  if (obj[name])
+  {
+    if (Array.isArray(obj[name]) == false)
+    {
+      var tmp = obj[name];
+      obj[name] = [];
+      obj[name].push(tmp);
+    }
+    var tmp = {};
+    obj[name].push(tmp);
+    obj = tmp;
+  }
+  else
+  {
+    obj[name] = {};
+    obj = obj[name];
+  }
+
   for (var i = 0; i < len; ++i)
   {
+    if (buf.length > 1000)  {     debug(buf);     buf = "";    }
     if (!i) buf += ": ";
     if (i) buf += ", ";
-    buf += parsePropertyRecord(cursor);
+    var val = parsePropertyRecord(cursor);
+    buf += val.toString();
+    obj[i] = val;
   }
-  log(buf);
+  debug(buf);
+
+  if (!end)    return false;
 
   if (cursor.offset != end)
   {
-    log("{");
+    debug("{");
     indent += "  ";
     while (cursor.offset != end)
     {
-      parseObjectRecord(cursor);
+      parseObjectRecord(obj, cursor);
       if (end - cursor.offset == 13)
       {
         for (var i = 0; i < 13; ++i)
-          if (cursor.data[cursor.offset + i] != 0) log("< expected null >");
+          if (cursor.data[cursor.offset + i] != 0) debug("< expected null >");
         cursor.offset += 13;
       }
     }
 
     indent = indent.substr(0, indent.length - 2);
-    log("}");
+    debug("}");
   }
+
+  return true;
 }
 
 function parsePropertyRecord(cursor)
 {
   var type = getChar(cursor);
-  if (type == 'Y') return ("(small integer) " + getSmallInteger(cursor));
-  else if (type == 'C') return ("(bool) " + getBool(cursor));
-  else if (type == 'I') return ("(integer) " + getInteger(cursor));
-  else if (type == 'F') return ("(float) " + getFloat(cursor));
-  else if (type == 'D') return ("(wide float) " + getWideFloat(cursor));
-  else if (type == 'L') return ("(wide integer) " + getWideInteger(cursor));
-  else if (type == 'f') return ("(float[]) " + getArrayInteger(cursor));
-  else if (type == 'd') return ("(wide float[]) " + getArrayWideInteger(cursor));
-  else if (type == 'l') return ("(wide integer[]) " + getArrayWideInteger(cursor));
-  else if (type == 'i') return ("(integer[]) " + getArrayInteger(cursor));
-  else if (type == 'b') return ("(bool[]) " + getArrayBool(cursor));
-  else if (type == 'S') return ("(string) " + getLongString(cursor));
-  else if (type == 'R') return ("(data) " + getData(cursor));
+  if (type == 'Y')      return (/* "(small integer) "  + */ getSmallInteger(cursor));
+  else if (type == 'C') return (/* "(bool) "           + */ getBool(cursor));
+  else if (type == 'I') return (/* "(integer) "        + */ getInteger(cursor));
+  else if (type == 'F') return (/* "(float) "          + */ getFloat(cursor));
+  else if (type == 'D') return (/* "(wide float) "     + */ getWideFloat(cursor));
+  else if (type == 'L') return (/* "(wide integer) "   + */ getWideInteger(cursor));
+  else if (type == 'f') return (/* "(float[]) "        + */ getArrayFloat(cursor));
+  else if (type == 'd') return (/* "(wide float[]) "   + */ getArrayWideFloat(cursor));
+  else if (type == 'l') return (/* "(wide integer[]) " + */ getArrayWideInteger(cursor));
+  else if (type == 'i') return (/* "(integer[]) "      + */ getArrayInteger(cursor));
+  else if (type == 'b') return (/* "(bool[]) "         + */ getArrayBool(cursor));
+  else if (type == 'S') return (/* "(string) "         + */ getLongString(cursor));
+  else if (type == 'R') return (/* "(data) "           + */ getData(cursor));
   else
     return "(missing type)";
 }
 
+var root = {};
+
 function process(data)
 {
   var bytes = new Uint8Array(data);
+  log("Got model data, size " + bytes.length);
+  log("Starting");
 
   // validate
   var header = "";
   for (var i = 0; i < 20; i++)
-    header += String.fromCharCode(parseInt(bytes[i], 2));
+    header += String.fromCharCode(bytes[i]);
 
   if (header != "Kaydara FBX Binary  ") log("Missing valid header");
   if (bytes[20] != 0x0) log("Missing magic number");
@@ -240,7 +362,16 @@ function process(data)
 
   // first step, just parse and output what we find
   var cursor = { data: bytes, offset: 23 };
-  log("File version:" + getInteger(cursor));
+  debug("File version:" + getInteger(cursor));
   while (cursor.offset != bytes.length)
-    parseObjectRecord(cursor);
+    if (!parseObjectRecord(root, cursor)) break;
+
+  log("Decoded");
+  root.Definitions = "";
+
+  var result = JSON.stringify(root)
+  log("result is " + result.length);
+
+  log("Done");
+  postMessage({ type: 1, result: result });
 }
